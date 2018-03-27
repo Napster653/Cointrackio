c8eef90eea5b4b7337aaef4f5f40990da1a2602d
Napster653 <100330656@alumnos.uc3m.es>
2018-03-27 13:28:29 +0200

    bittrex creation

--- traders/bittrex.js
+++ traders/bittrex.js
@@ -1,4 +1,4 @@
-function getProductsGdax()
+function getProductsBittrex()
 {
 	var request = new XMLHttpRequest;
 	var key;
@@ -7,22 +7,25 @@ function getProductsGdax()
 		if (this.readyState == 4 && this.status == 200)
 		{
 			var res = JSON.parse(request.responseText);
-			console.log(res);
-			document.getElementById("dataTable")
-			for (key in res)
+			if (res.success == true)
 			{
-				var productData = {
-					id: key,
-					high: res[key].stats_24hour.high,
-					low: res[key].stats_24hour.low,
-					volume: res[key].stats_24hour.volume,
-					last: res[key].stats_24hour.last
-				};
-				console.log(productData)
-				updateDataTable(productData);
+				console.log(res);
+				document.getElementById("dataTable")
+				for (key in res.result)
+				{
+					var productData = {
+						id: res[key].MarketName,
+						high: res[key].High,
+						low: res[key].Low,
+						volume: res[key].Volume,
+						last: res[key].Last
+					};
+					console.log(productData)
+					updateDataTable(productData);
+				}
 			}
 		}
 	}
-	request.open("GET", "https://api.gdax.com/products/stats", true);
+	request.open("GET", "https://bittrex.com/api/v1.1/public/getmarketsummaries", true);
 	request.send();
 }
